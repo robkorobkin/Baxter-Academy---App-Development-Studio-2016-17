e19607e48300ac0c7e6a707d5763a895ba355ce7
Hunter Howe <Hunter.howe@baxter-academy.org>
2016-11-04 13:42:43 -0400

    fixed ba app

---
+++ Students/Hunter/main.js
@@ -0,0 +1,24 @@
+var mainState = {
+	preload: function() {
+	
+	},
+
+	create: function() {
+
+
+	},
+
+	update: function() n{
+
+
+	},
+};
+
+
+var game = new Phaser.Game(400, 490);
+
+
+game.state.add('main', mainState):
+
+
+game.state.add('main');
